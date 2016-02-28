507bcd588c876545f69f34be0520da3c586f9a47
Mathieu Legrand <mathieu@legrand.im>
2016-02-29 00:01:09 +0800

    All tabs in the tabbar point to the same route

--- app/root.js
+++ app/root.js
@@ -11,6 +11,28 @@ import CustomNavBar from './customNavBar.js';
 export default class Root extends React.Component {
   closeDrawer = () => { this.refs.drawer.close() };
   openDrawer  = () => { this.refs.drawer.open()  };
+
+  constructor() {
+    super();
+    this.state = {
+      selectedTab: 'Page'
+    };
+  }
+
+  _navigator(route) {
+    return (
+      <ExNavigator
+        showNavigationBar={true}
+        initialRoute={ route }
+        style={{ flex: 1 }}
+        sceneStyle={{ paddingTop: 64 }}
+        openDrawer={ this.openDrawer }
+        closeDrawer={ this.closeDrawer }
+        renderNavigationBar={ props => <CustomNavBar {...props} /> }
+      />
+    );
+  }
+
   render() {
     return (
       <Drawer ref="drawer"
@@ -22,20 +44,33 @@ export default class Root extends React.Component {
               content={<React.View />}>
         <React.TabBarIOS style={{ height: 10 }}>
           <Icon.TabBarItem title="Page"
-                           selected={true}
-                           iconName="document"
-                           selectedIconName="document-text">
-            <ExNavigator
-              showNavigationBar={true}
-              initialRoute={Router.getWelcomeRoute()}
-              style={{ flex: 1 }}
-              sceneStyle={{ paddingTop: 64 }}
-              openDrawer={ this.openDrawer }
-              closeDrawer={ this.closeDrawer }
-              renderNavigationBar={ props => <CustomNavBar {...props} /> }
-            />
+                           selected={ this.state.selectedTab === 'Page'}
+                           onPress={() => { this.setState({ selectedTab: 'Page' }); }}
+                           iconName="ios-list-outline"
+                           selectedIconName="ios-list">
+            { this._navigator(Router.getWelcomeRoute()) }
           </Icon.TabBarItem>
-
+          <Icon.TabBarItem title="New Post"
+                           selected={this.state.selectedTab === 'New'}
+                           onPress={() => { this.setState({ selectedTab: 'New' }); }}
+                           iconName="ios-plus-outline"
+                           selectedIconName="ios-plus">
+            { this._navigator(Router.getWelcomeRoute()) }
+          </Icon.TabBarItem>
+          <Icon.TabBarItem title="Share"
+                           selected={this.state.selectedTab === 'Share'}
+                           onPress={() => { this.setState({ selectedTab: 'Share' }); }}
+                           iconName="ios-upload-outline"
+                           selectedIconName="ios-upload">
+            { this._navigator(Router.getWelcomeRoute()) }
+          </Icon.TabBarItem>
+          <Icon.TabBarItem title="Settings"
+                           selected={this.state.selectedTab === 'Settings'}
+                           onPress={() => { this.setState({ selectedTab: 'Settings' }); }}
+                           iconName="ios-cog-outline"
+                           selectedIconName="ios-cog">
+            { this._navigator(Router.getWelcomeRoute()) }
+          </Icon.TabBarItem>
         </React.TabBarIOS>
       </Drawer>
     );
