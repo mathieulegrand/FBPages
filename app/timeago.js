374575c3ca888fc1c2041ca7ac5610cc4a87537d
Mathieu Legrand <mathieu@legrand.im>
2016-03-02 10:50:32 +0800

    Fix the timer

--- app/timeago.js
+++ app/timeago.js
@@ -1,5 +1,6 @@
 // originally from https://github.com/TylerLH/react-native-timeago
-// but simplified a lot and using calendar() for the display method
+// but simplified as discussed on https://github.com/reactjs/react-timer-mixin/issues/4
+// and using calendar() for the display method
 
 import React  from 'react-native';
 import Moment from 'moment';
@@ -10,15 +11,11 @@ export default class TimeAgo extends React.Component {
   }
 
   componentDidMount() {
-    if (this.props.interval > 0) {
-      setInterval(this.update, this.props.interval);
-    }
+    this.timer = setTimeout(() => { this.forceUpdate() }, this.props.interval);
   }
 
   componentWillUnmount() {
-    if (this.update) {
-      clearInterval(this.update);
-    }
+    clearTimeout(this.timer);
   }
 
   render() {
